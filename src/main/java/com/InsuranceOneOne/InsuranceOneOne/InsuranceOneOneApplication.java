37d83d682852983c1b1a68ae550a5b0b68ab4cda
Samuel Callu-Wiatr <117657697+SamuelCallWind@users.noreply.github.com>
2025-05-14 15:27:01 +0200

    Initial commit

---
+++ src/main/java/com/InsuranceOneOne/InsuranceOneOne/InsuranceOneOneApplication.java
@@ -0,0 +1,13 @@
+package com.InsuranceOneOne.InsuranceOneOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceOneOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceOneOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InsuranceOneOne/InsuranceOneOne/InsuranceOneOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InsuranceOneOne.InsuranceOneOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceOneOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
