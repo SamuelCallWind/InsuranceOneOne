3531c1851c4d50dd9e72ac1547b13e99f839c841
Samuel Callu <Samcallwind@gmail.com>
2025-07-04 22:12:44 +0200

    Claim class added

--- src/main/java/com/InsuranceOneOne/entity/Claim.java
+++ src/main/java/com/InsuranceOneOne/entity/Claim.java
@@ -1,5 +1,36 @@
 package com.InsuranceOneOne.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "claims")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Claim {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate claimDate;
+
+    @Column(nullable = false)
+    private BigDecimal claimAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
 }
