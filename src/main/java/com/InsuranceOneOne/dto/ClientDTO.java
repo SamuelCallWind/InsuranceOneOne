9c56aa801e2c7c24d177e1fa898789b9007cb9d2
Samuel Callu <Samcallwind@gmail.com>
2025-07-06 08:51:18 +0200

    added client DTO and start of authController

---
+++ src/main/java/com/InsuranceOneOne/dto/ClientDTO.java
@@ -0,0 +1,14 @@
+package com.InsuranceOneOne.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientDTO {
+
+    private String email;
+    private String password;
+}
